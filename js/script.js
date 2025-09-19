1dbe977038d98632e13a88014406d6aab52855e2
kendriks <kendriksdapaixao@gmail.com>
2025-09-19 17:09:56 -0300

    feat: versão 1

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//pegar animes da api
+
+//armazena o elemento com id "lista-animes" na variavel main
+const main = document.getElementById("lista-animes");
+
+//url da api
+const url = "https://api.jikan.moe/v4/top/anime";
+
+
+
+
